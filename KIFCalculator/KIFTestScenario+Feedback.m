40a2015d5b4fd5f97cd6bc732565ad77ba8b151a
Chaise Hocking <Chaise.Hocking@gmail.com>
2012-08-24 02:02:46 +1000

    Cleaned up indentation

--- KIFCalculator/KIFTestScenario+Feedback.m
+++ KIFCalculator/KIFTestScenario+Feedback.m
@@ -15,15 +15,24 @@
 +(id)scenarioToTestThatSubmitingTheFeedbackFormPostsTheFeedback{
 	KIFTestScenario *scenario = [KIFTestScenario scenarioWithDescription:@"Test that the feedback form is posted when the submit button is pressed"];
 	[scenario addStep:[KIFTestStep stepToTapViewWithAccessibilityLabel:@"Feedback" traits:UIAccessibilityTraitButton]];
-	[scenario addStep:[KIFTestStep stepToEnterText:@"Joe Blogs" intoViewWithAccessibilityLabel:@"Name" traits:UIAccessibilityTraitNone expectedResult:@"Joe Blogs"]];
-	[scenario addStep:[KIFTestStep stepToEnterText:@"Joe.Blogs@gmail.com" intoViewWithAccessibilityLabel:@"Email" traits:UIAccessibilityTraitNone expectedResult:@"Joe.Blogs@gmail.com"]];
+	
+	[scenario addStep:[KIFTestStep stepToEnterText:@"Joe Blogs"
+					intoViewWithAccessibilityLabel:@"Name"
+											traits:UIAccessibilityTraitNone
+									expectedResult:@"Joe Blogs"]];
+	
+	[scenario addStep:[KIFTestStep stepToEnterText:@"Joe.Blogs@gmail.com"
+					intoViewWithAccessibilityLabel:@"Email"
+											traits:UIAccessibilityTraitNone
+									expectedResult:@"Joe.Blogs@gmail.com"]];
 	
 	[scenario addStep:[KIFTestStep stepToSwipeViewWithAccessibilityLabel:@"Feedback Form" inDirection:KIFSwipeDirectionUp]];
-	
 	[scenario addStep:[KIFTestStep stepToSetOn:YES forSwitchWithAccessibilityLabel:@"Require Response"]];
 	
 	NSString *message = @"Hi,\nI just wanted to tell you that Swipe Conference 2012 ROCKS!";
-	[scenario addStep:[KIFTestStep stepToEnterText:message intoViewWithAccessibilityLabel:@"Feedback Message" traits:UIAccessibilityTraitNone expectedResult:message]];
+	[scenario addStep:[KIFTestStep stepToEnterText:message
+					intoViewWithAccessibilityLabel:@"Feedback Message"
+											traits:UIAccessibilityTraitNone expectedResult:message]];
 	
 	[scenario addStep:[KIFTestStep stepToWaitForNotificationName:@"kFeedbackPostedNotification"
 														  object:nil
