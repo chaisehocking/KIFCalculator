3fb1b598cd4bb80e3cf40e8a57eb2003f3673cea
Chaise Hocking <Chaise.Hocking@gmail.com>
2012-08-27 21:19:36 +1000

    Force Fail a scenario

--- KIFCalculator/KIFTestScenario+CalculatorOperations.m
+++ KIFCalculator/KIFTestScenario+CalculatorOperations.m
@@ -25,7 +25,7 @@
 	[scenario addStep:[KIFTestStep stepToTapViewWithAccessibilityLabel:@"4" traits:UIAccessibilityTraitButton]];
 	[scenario addStep:[KIFTestStep stepToTapViewWithAccessibilityLabel:@"=" traits:UIAccessibilityTraitButton]];
 	[scenario addStep:[KIFTestStep stepToWaitForViewWithAccessibilityLabel:@"Answer"
-																	 value:@"2"
+																	 value:@"23"
 																	traits:UIAccessibilityTraitStaticText]];
 	return scenario;
 }
